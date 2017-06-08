566cf051f93b555e72152a59666f42edce2cab04
Maximilian Gustafsson <max.r.gus@gmail.com>
2017-06-08 19:45:58 +0200

    Added TagsController with Get action

---
+++ QLLAB/Controllers/TagsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLLAB.Controllers
+{
+    [Route("api/[controller]")]
+    public class TagsController : Controller
+    {
+        [Route("")]
+        public IActionResult Get()
+        {
+            string[] tags = { "Tag1", "Tag2", "Tag3", "Tag4" };
+
+            return Ok(tags);
+
+        }
+
+    }
+}

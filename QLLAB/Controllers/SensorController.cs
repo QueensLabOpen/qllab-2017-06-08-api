314303773747ec72ed3a092a9825462068561ba6
Maximilian Gustafsson <max.r.gus@gmail.com>
2017-06-08 19:55:05 +0200

    Sensor controller with Set action

---
+++ QLLAB/Controllers/SensorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLLAB.Controllers
+{   
+    [Route("api/[controller]")]
+    public class SensorController : Controller
+    {
+        [HttpGet]
+        [Route("set/{deviceId}/{sensorId}/{active}")]
+        public IActionResult Set(string deviceId, string sensorId, bool active)
+        {
+            return Ok($"{deviceId}/{sensorId}/{active}");
+        }
+    }
+}

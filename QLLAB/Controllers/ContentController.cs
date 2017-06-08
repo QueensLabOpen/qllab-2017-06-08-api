242e5a8ff473a154e181440f8ea28eacc90260d6
Marcus Nordquist <marcus.nordquist@keenear.se>
2017-06-08 19:16:43 +0200

    » Init

---
+++ QLLAB/Controllers/ContentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLLAB.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContentController : Controller
+    {
+        [Route("")]
+        public IActionResult Get()
+        {
+            return Ok("Tjena");
+        }
+    }
+}
